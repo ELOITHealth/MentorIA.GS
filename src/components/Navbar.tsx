1c310a3f9d4cb60bcfeaf680f6a7ad28a8ae4327
Matheus <RM562480@fiap.com.br>
2025-11-23 16:52:04 -0300

    Adicionado navHover.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./navHover.css"
 
 export default function Navbar() {
   const [theme, setTheme] = useState<'light' | 'dark'>(() => {
